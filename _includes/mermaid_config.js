e8f5631d261a05aa06974ec0922aac0af1d9c59d
CouncilofLorien <50040455+CouncilofLorien@users.noreply.github.com>
2022-11-15 11:21:57 -0800

    Update mermaid_config.js

--- _includes/mermaid_config.js
+++ _includes/mermaid_config.js
@@ -1,1 +1,9 @@
-{}
+{
+  "theme": "default",
+  "themeVariables": {
+    "nodeBorder" : "#004990",
+    "mainBkg" : "#c9d7e4",
+    "nodeTextColor" : "#274059",
+    "fontFamily": "timesnewroman",
+    "fontSize": "16px"
+ }
